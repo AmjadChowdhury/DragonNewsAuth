b51ceed2f2b9ae6c2893b0c13fbe16d1771f24cf
AmjadChowdhury <amjadchowdhury247@gmail.com>
2024-04-10 08:01:58 +0100

    proptypes add for children

--- src/Provider/AuthProvider.jsx
+++ src/Provider/AuthProvider.jsx
@@ -1,6 +1,7 @@
 import { createUserWithEmailAndPassword, onAuthStateChanged, signInWithEmailAndPassword, signOut } from "firebase/auth";
 import { createContext, useEffect, useState } from "react";
 import auth from "../firebase/firebase.config";
+import PropTypes from 'prop-types'
 
 export const AuthContext = createContext(null)
 
@@ -49,5 +50,9 @@ const AuthProvider = ({children}) => {
         </AuthContext.Provider>
     );
 };
+
+AuthProvider.propTypes = {
+    children : PropTypes.node
+}
 
 export default AuthProvider;
